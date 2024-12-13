b6a99d04af5671e4db29531f3dafae7a616d3e97
RubenValentin <rubenvalentin.caravaca@murciaeduca.es>
2024-12-13 18:23:55 +0100

    update

---
+++ 9_conditionals.py
@@ -0,0 +1,40 @@
+
+
+# Conditionals
+
+# if
+
+# my_condition = True
+
+# if my_condition:
+#     print("se ejecuta la condicion if")
+
+# my_condition = 5 * 5
+# var2 = 0
+# if my_condition == 10:
+#     print("se ejecuta la condicion if")
+#     var2 = 5+7
+#     print("Imprimiendo la variable 2", var2)
+
+# var1 = 6
+
+# print(var2)
+
+# if, elif , else
+
+# print("--------------------------")
+# my_condition = int(input("Dame un numero, no letras: "))
+
+# if my_condition > 10 and my_condition < 20:
+#     print("Es mayor que 10 y menor que 20")
+# elif my_condition == 25:
+#     print("Es igual a 25")
+# else:
+#     print("Es menor o igual que 10 o mayor o igual a 20")
+
+# print("Fin de programa")
+
+
+my_string = ""
+if not my_string:
+    print("Mi cadena de texto esta vacia")
